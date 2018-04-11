eafea5737d22133bcb42c9fcd8d573336a02a4af
fengzhou <fengzhou@youzan.com>
2018-04-11 12:41:54 +0800

    update config.py and create models file

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 class Config(object):
     DEBUT = True
+
+class DBConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:fengzhou@127.0.0.1:3306/fz'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
